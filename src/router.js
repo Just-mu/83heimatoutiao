f50a2f9935cb9d4a0620b19480cdd8c42bcaa19c
穆江浩 <54099464+Just-mu@users.noreply.github.com>
2019-09-19 14:53:15 +0800

    第一阶段

--- src/router.js
+++ src/router.js
@@ -1,23 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+// import Home from './views/home/index.vue' // 完整写法
+import Home from './views/home/'
+import Login from './views/login' // 简写
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // 一级路由
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: '/home'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/home',
+      component: Home
+    }, {
+      path: '/login',
+      component: Login
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue') // 按需加载
+    // }
   ]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,33 @@
+<template>
+<!-- 先定义一个大容器 -->
+  <el-container>
+    <!-- 先放置一个左侧 -->
+    <el-aside>
+      <!-- 左侧导航组件 -->
+      <layout-aside></layout-aside>
+    </el-aside>
+    <!-- 右侧大容器 -->
+    <el-container>
+      <!-- 头部 -->
+      <el-header>头部</el-header>
+      <!-- 中部区域 -->
+      <el-main>
+        <!-- 二级路由容器 -->
+         <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import layoutAside from '../../components/home/layout-aside'
+export default {
+  components: {
+    'layout-aside': layoutAside
+  }
+}
+</script>
+
+<style>
+
+</style>
